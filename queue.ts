a479bba1fe403feab6eaa1c5391bf4b33e80adb2
Jeremiah Langner <jlangner@gsu.edu>
2024-03-22 19:21:28 -0700

    add queue

--- queue.ts
+++ queue.ts
@@ -1,6 +1,56 @@
+import { assert } from 'assert';
+
 class Node<T> {
   value: T;
+  next?: Node<T>;
 
-  constructor(value: T) {
+  constructor(value: T, next?: Node<T>) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class Queue<T> {
+  length: number;
+  head?: Node<T>;
+  tail?: Node<T>;
+
+  constructor() {
+    this.tail = this.head = undefined;
+    this.length = 1;
+  }
+
+  enqueue(value: T) {
+    if (!this.tail) {
+      this.tail = this.head = new Node(value);
+      this.length = 1;
+    } else {
+      this.tail.next = new Node(value);
+      this.length++;
+    }
+  }
+
+  dequeue(): T | undefined {
+    if (!this.head) {
+      return undefined;
+    }
+
+    this.length--;
+
+    const head = this.head;
+    if (this.tail === this.head) this.tail = undefined;
+    this.head = this.head.next;
+
+    head.next = undefined;
+
+    return head.value;
+  }
+
+  peek(): T | undefined {
+    if (!this.head) {
+      return undefined;
+    } else {
+      return this.tail!.value;
+    }
   }
 }
