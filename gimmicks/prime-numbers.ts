75fb439de144ce709c164fa5b80bbf0cce4107f0
Jeremiah Langner <jlangner@gsu.edu>
2024-04-08 09:20:10 -0700

    modify sieve given roots

--- gimmicks/prime-numbers.ts
+++ gimmicks/prime-numbers.ts
@@ -22,17 +22,17 @@ function sieve(limit: number): number[] {
   console.log(performance.now());
   const isPrime: boolean[] = new Array(limit);
   isPrime.fill(true);
-  const primes: number[] = [];
-  for (let i = 2; i < limit; ++i) {
-    if (!isPrime[i]) continue;
-    for (let j = i; j < limit; j += i) {
-      isPrime[j] = false;
+  isPrime[0] = isPrime[1] = false;
+  for (let i = 2; i * i < limit; ++i) {
+    if (isPrime[i]) {
+      for (let j = i * i; j < limit; j += i) {
+        isPrime[j] = false;
+      }
     }
-    primes.push(i);
   }
-  console.log(performance.now());
-  return primes;
+  console.log('erato', performance.now());
+  return isPrime.map((item, i) => item ? i : 0).filter(i => i !== 0);
 }
 
-console.log('simple', simple(1e5));
-console.log('sieve', sieve(1e5));
+console.log('simple', simple(1e6));
+console.log('sieve', sieve(1e6));
